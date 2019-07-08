0bae7cfb1fcd19237b3e7397e8654f41d69132c2
sweetrain096 <gpfhddl09@gmail.com>
2019-07-08 10:19:28 +0900

    rain|add vuetify
    vuetify 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,
