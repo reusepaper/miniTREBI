1daf099189a65376f24b77f8b7f4b12d9f2c75b9
한만섭 <13akstjq@naver.com>
2019-07-08 17:24:10 +0900

    mansub | data change function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,6 @@
   <div>
     <WMHeader></WMHeader>
     <v-container>
-<<<<<<< HEAD
-      <HelloWorld/>
-    </v-container>
-    <WMSidebar></WMSidebar>
-=======
       <v-layout>
         <v-flex xs3>
           <WMSidebar></WMSidebar>
@@ -17,7 +12,6 @@
       </v-layout>
     </v-container>
 
->>>>>>> 942683708c2670b55061af9c8bbce06bef230d9a
     <WMFooter></WMFooter>
   </div>
 </template>
