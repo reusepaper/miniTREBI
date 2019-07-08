dea2acbd90bf95bd7e00fcf9219896ccc6db5e7e
YeonjiLim <reusepaper210@gmail.com>
2019-07-08 17:06:18 +0900

    yeon|Change WMHeader UI
    인원이 5명이라 창을 줄이면 안예뻐서 드롭다운 형태로 작가를 선택하게 바꿨습니다

--- src/components/WMHeader.vue
+++ src/components/WMHeader.vue
@@ -1,26 +1,42 @@
 <template>
-  <div style="position: relative;">
-    <v-toolbar fixed>
-      <v-btn flat icon>
-          <v-icon>home</v-icon>
-      </v-btn>
-      <v-toolbar-title>TREVI</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>LOGIN</v-btn>
-        <v-btn flat>Ho</v-btn>
-        <v-btn flat>DK</v-btn>
-        <v-btn flat>yeon</v-btn>
-        <v-btn flat>mansub</v-btn>
-        <v-btn flat>rain</v-btn>
-        <v-btn flat>Search</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  </div>
+<div style="position: relative;">
+  <v-toolbar fixed dense>
+    <v-btn flat icon>
+      <v-icon>home</v-icon>
+    </v-btn>
+    <v-toolbar-title>TREVI</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn flat>LOGIN</v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn v-on="on" flat>
+            Writer
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-tile v-for="(item, index) in items" :key="index" @click="">
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-btn flat>Search</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'WMHeader'
+  name: 'WMHeader',
+  data: () => ({
+        items: [
+          { title: 'Ho' },
+          { title: 'ydk' },
+          { title: 'yeon' },
+          { title: 'mansub' },
+          { title: 'rain'}
+        ]
+      })
 }
 </script>
