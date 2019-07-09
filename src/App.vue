9fdc92751ccdde1faa1f517a291cce60210fbac2
leeiopd <leeiopd@hanmail.net>
2019-07-09 10:47:32 +0900

    ho|error

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <v-app>
     <v-content>
       <Header></Header>
-      <router-view/>
+      <router-view />
       <back-to-top text="Back to top">
-        <button type='button' class='v-btn v-btn--bottom v-btn--floating v-btn--fixed v-btn--right theme--dark red'>
-          <i aria-hidden='true' class='v-icon material-icons theme--dark'>keyboard_arrow_up</i>
+        <button
+          type="button"
+          class="v-btn v-btn--bottom v-btn--floating v-btn--fixed v-btn--right theme--dark red"
+        >
+          <i aria-hidden="true" class="v-icon material-icons theme--dark">keyboard_arrow_up</i>
         </button>
       </back-to-top>
       <Footer></Footer>
@@ -14,26 +17,26 @@
 </template>
 
 <script>
-import store from './store'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import store from "./store";
+import Header from "./components/WMHeader";
+import Footer from "./components/WMFooter";
 
 export default {
-	name: 'App',
-  components:{
+  name: "App",
+  components: {
     Header,
     Footer
   },
-	store,
-	data() {
-		return {
-			//
-		}
-	},
+  store,
+  data() {
+    return {
+      //
+    };
+  },
   beforeCreate() {
- if(browser !== 'Chrome'){
-  alert("본 사이트는 크롬버전에 최적화되어있습니다.")
- }
-}
-}
+    if (browser !== "Chrome") {
+      alert("본 사이트는 크롬버전에 최적화되어있습니다.");
+    }
+  }
+};
 </script>
