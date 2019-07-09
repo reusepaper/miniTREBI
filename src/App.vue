469c21f33e1247adabb66dff416e51aad5c64b94
이주호 <leeiopd@hanmail.net>
2019-07-09 01:43:44 +0000

    Revert "Merge branch 'ydk' into 'master'"
    This reverts merge request !28

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <template>
   <v-app>
-    <v-content>
-      <Header></Header>
-      <router-view/>
-      <back-to-top text="Back to top">
-        <button type='button' class='v-btn v-btn--bottom v-btn--floating v-btn--fixed v-btn--right theme--dark red'>
-          <i aria-hidden='true' class='v-icon material-icons theme--dark'>keyboard_arrow_up</i>
-        </button>
-      </back-to-top>
-      <Footer></Footer>
-    </v-content>
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-import store from './store'
-import Header from './components/Header'
-import Footer from './components/Footer'
-
 export default {
-	name: 'App',
-  components:{
-    Header,
-    Footer
-  },
-	store,
-	data() {
-		return {
-			//
-		}
-	},
-  beforeCreate() {
- if(browser !== 'Chrome'){
-  alert("본 사이트는 크롬버전에 최적화되어있습니다.")
- }
-}
-}
+  name: "App",
+
+  data() {
+    return {
+      //
+    };
+  }
+};
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
 <template>
   <v-container>
     <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img>
+      </v-flex>
+
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">PJT2 트래비 블로그에 오신것을 환영합니다.</h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
@@ -59,8 +63,6 @@
 </template>
 
 <script>
-import ImgBanner from './ImgBanner'
-
 export default {
   data: () => ({
     ecosystem: [
