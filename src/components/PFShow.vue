c4750bb3fb8867d4798a294e11730751c52b7d85
한만섭 <13akstjq@naver.com>
2019-07-10 17:40:06 +0900

    mansub | position center bottom

--- src/components/PFShow.vue
+++ src/components/PFShow.vue
@@ -1,13 +1,12 @@
 <template>
   <v-layout>
     <v-flex xs3 sm6 px-2>
-      <v-card class='card-img-1'>
-        <v-img
-        :src="require('../assets/goubook1.png')" aspect-ratio="1"></v-img>
+      <v-card class="card-img-1">
+        <v-img :src="require('../assets/goubook1.png')" aspect-ratio="1" position="center bottom"></v-img>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">유동관</h3>
-            <div> {{ card_text }} </div>
+            <div>{{ card_text }}</div>
           </div>
         </v-card-title>
 
@@ -17,14 +16,13 @@
         </v-card-actions>
       </v-card>
     </v-flex>
-      <v-flex xs3 sm6 px-2>
-      <v-card class='card-img-1'>
-        <v-img
-        :src="require('../assets/pikachu.jpg')" aspect-ratio="1"></v-img>
+    <v-flex xs3 sm6 px-2>
+      <v-card class="card-img-1">
+        <v-img :src="require('../assets/pikachu.jpg')" aspect-ratio="1" position="center bottom"></v-img>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">한단비</h3>
-            <div> {{ card_text }} </div>
+            <div>{{ card_text }}</div>
           </div>
         </v-card-title>
 
@@ -34,14 +32,13 @@
         </v-card-actions>
       </v-card>
     </v-flex>
-        <v-flex xs3 sm6 px-2>
-      <v-card class='card-img-1'>
-        <v-img
-        :src="require('../assets/hesee.jpg')" aspect-ratio="1"></v-img>
+    <v-flex xs3 sm6 px-2>
+      <v-card class="card-img-1">
+        <v-img :src="require('../assets/hesee.jpg')" aspect-ratio="1" position="center bottom"></v-img>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">임연지</h3>
-            <div> {{ card_text }} </div>
+            <div>{{ card_text }}</div>
           </div>
         </v-card-title>
 
@@ -51,14 +48,13 @@
         </v-card-actions>
       </v-card>
     </v-flex>
-        <v-flex xs3 sm6 px-2>
-      <v-card class='card-img-1'>
-        <v-img
-        :src="require('../assets/togepi.jpg')" aspect-ratio="1"></v-img>
+    <v-flex xs3 sm6 px-2>
+      <v-card class="card-img-1">
+        <v-img :src="require('../assets/togepi.jpg')" aspect-ratio="1" position="center bottom"></v-img>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">이주호</h3>
-            <div> {{ card_text }} </div>
+            <div>{{ card_text }}</div>
           </div>
         </v-card-title>
 
@@ -68,14 +64,13 @@
         </v-card-actions>
       </v-card>
     </v-flex>
-        <v-flex xs3 sm6 px-2>
-      <v-card class='card-img-1'>
-        <v-img
-        :src="require('../assets/jammanbo.jpg')" aspect-ratio="1"></v-img>
+    <v-flex xs3 sm6 px-2>
+      <v-card class="card-img-1">
+        <v-img :src="require('../assets/jammanbo.jpg')" aspect-ratio="1" position="center bottom"></v-img>
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">한만섭</h3>
-            <div> {{ card_text }} </div>
+            <div>{{ card_text }}</div>
           </div>
         </v-card-title>
 
@@ -85,26 +80,27 @@
         </v-card-actions>
       </v-card>
     </v-flex>
-    
   </v-layout>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
-      }
-    }
+export default {
+  data() {
+    return {
+      card_text:
+        "Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat."
+    };
   }
+};
 </script>
 <style>
-.card-img-1, .card-img-2{
+.card-img-1,
+.card-img-2 {
   overflow: hidden;
 }
-.card-img-1 img{
+.card-img-1 img {
   margin-top: -60%;
 }
-.card-img-2 img{
+.card-img-2 img {
   margin-top: -68%;
 }
 </style>
