50cf1e871ade10938aad4d7a0973f2432ca99304
leeiopd <leeiopd@hanmail.net>
2019-07-10 14:50:27 +0900

    ho|firebase page log
    test중

--- src/components/WMHeader.vue
+++ src/components/WMHeader.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="hide-overflow" style="position: relative;">
+  <div class="hide-overflow" style="position: relative;">
     <v-toolbar fixed color="primary lighten-3">
       <v-btn flat icon to="/" color="white">
         <v-icon>home</v-icon>
@@ -9,20 +9,19 @@
       <v-toolbar-items class="hidden-xs-only">
         <!-- 로그인 다이얼로그 활성화 -->
         <v-btn flat color="white" @click.stop="login_btn = true">Login</v-btn>
-          <v-dialog v-model="login_btn" max-width="290">
-            <v-card>
-              <v-card-title class="headline">Log in</v-card-title>
-              <v-card-text>
-                <SignIn></SignIn>
-                SignIn template
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Disagree</v-btn>
-                <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Agree</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+        <v-dialog v-model="login_btn" max-width="290">
+          <v-card>
+            <v-card-title class="headline">Log in</v-card-title>
+            <v-card-text>
+              <SignIn></SignIn>SignIn template
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Disagree</v-btn>
+              <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Agree</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <v-btn to="/post" class="white--text" flat>Post</v-btn>
         <v-btn to="/portfolio" class="white--text" flat>Portfolio</v-btn>
         <!-- sdfsdfsdfsdfsd-->
@@ -31,20 +30,19 @@
             <v-btn v-on="on" flat color="white">Writer</v-btn>
           </template>
           <v-list>
-            <v-list-tile v-for="(item, index) in items" :key="index" @click="">
+            <v-list-tile v-for="(item, index) in items" :key="index" @click>
               <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile>
           </v-list>
         </v-menu>
 
-
         <!-- <v-dialog v-model="dialog" width="500"> -->
-          <!-- <template v-slot:activator="{ on }">
+        <!-- <template v-slot:activator="{ on }">
             <v-btn class="white--text" flat v-on="on">
               <v-icon >star</v-icon>
             </v-btn>
-          </template> -->
-          <!-- <v-card>
+        </template>-->
+        <!-- <v-card>
             <v-card-title class="headline primary lighten-3 white--text">
               안내
             </v-card-title>
@@ -55,7 +53,7 @@
               <v-spacer></v-spacer>
               <v-btn color="primary lighten-3" class="white--text" @click="dialog = false">확인</v-btn>
             </v-card-actions>
-          </v-card> -->
+        </v-card>-->
         <!-- </v-dialog> -->
       </v-toolbar-items>
       <v-toolbar-side-icon class="hidden-sm-and-up white--text" @click="drawer = !drawer"></v-toolbar-side-icon>
@@ -79,8 +77,8 @@
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
-    </v-navigation-drawer> -->
-</div>
+    </v-navigation-drawer>-->
+  </div>
 </template>
 
 <script>
@@ -101,14 +99,12 @@ export default {
     dialog: false,
     login_btn: false
   }),
-  methods:{
-    showLoginDialog:function(){
-
-    }
+  methods: {
+    showLoginDialog: function() {}
   },
-  mounted: function(){
-    console.log(sessionStorage['length'])
-    let isLogin = sessionStorage['length']
+  mounted: function() {
+    console.log(sessionStorage["length"]);
+    let isLogin = sessionStorage["length"];
   }
 };
 </script>
