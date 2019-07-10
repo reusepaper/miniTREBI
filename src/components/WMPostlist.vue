08a3275d1bfb78a22a4c73d41bfa7b7e72ed41d6
YeonjiLim <reusepaper210@gmail.com>
2019-07-10 10:31:58 +0900

    Edit page

--- src/components/WMPostlist.vue
+++ src/components/WMPostlist.vue
@@ -1,12 +1,15 @@
 <template>
   <v-layout mt-5>
-    <v-flex v-for="i in posts.length > limits ? limits : posts.length" x12 sm6 md3>
+    <v-flex v-for="i in posts.length > limits ? limits : posts.length" xs12>
       <WMPost class="ma-3"
         :title="posts[i-1].title"
         :body="posts[i-1].content"
         :image="posts[i-1].image"
       >
       </WMPost>
+    </v-flex>
+    <v-flex xs12 text-xs-center round my-5 v-if="loadMore">
+      <v-btn v-on:click="loadMorePosts"> 더 보기</v-btn>
     </v-flex>
   </v-layout>
 </template>
@@ -18,7 +21,8 @@ import FirebaseService from '@/services/FirebaseService'
 export default {
   name: 'WMPostlist',
   props: {
-    limits: {type: Number, default: 10}
+    limits: {type: Number, default: 4},
+    loadMore: {type: Boolean, default: false}
   },
   data() {
     return {
@@ -34,6 +38,9 @@ export default {
   methods: {
     async getPosts(){
       this.posts = await FirebaseService.getPosts()
+    },
+    loadMorePosts(){
+      this.limits += 4;
     }
   }
 }
