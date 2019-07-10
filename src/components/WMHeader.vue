0a918de79d7c4a83a345936e047d7d7ca125657c
youdongkwan <dkyou7@naver.com>
2019-07-10 10:32:34 +0900

    YDK | navbar update
    bootstrap -> vuetify

--- src/components/WMHeader.vue
+++ src/components/WMHeader.vue
@@ -1,119 +1,99 @@
 <template>
-  <nav class="navbar shadow fixed-top navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-      <img src="../assets/logo.svg" width="30" height="30" class="d-inline-block align-top" alt />
-      TREVI
-    </a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-<<<<<<< HEAD
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">
-            Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a
-            href="#"
-            class="nav-link hi"
-            title="즐겨찾기 등록"
-            onclick="alert('ctrl+d버튼을 눌러 저장할 수 있습니다.')"
-          >
-            <img src="../assets/bookmark-black-shape.svg" style="width:10px;" />
-          </a>
-        </li>
-      </ul>
-      <span class="navbar-text">Navbar text with an inline element</span>
-    </div>
-    <div style="position: relative;">
-      <v-toolbar fixed dense>
-        <v-btn flat icon>
-          <v-icon>home</v-icon>
-        </v-btn>
-        <v-toolbar-title>TREVI</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-xs-only">
-          <v-btn flat @click.stop="dialog = true">LOGIN</v-btn>
-          <v-dialog v-model="dialog" max-width="290">
-            <v-card>
-              <v-card-title class="headline">Log in</v-card-title>
-=======
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link hi" title="즐겨찾기 등록" onclick="alert('ctrl+d버튼을 눌러 저장할 수 있습니다.')"><img src="../assets/bookmark-black-shape.svg" style="width:10px;"/></a>
-      </li>
-    </ul>
-    <span class="navbar-text">
-      Navbar text with an inline element
-    </span>
-  </div>
-  <div style="position: relative;">
-    <v-toolbar fixed dense>
-      <v-btn flat icon>
+<div class="hide-overflow" style="position: relative;">
+    <v-toolbar fixed color="primary lighten-3">
+      <v-btn flat icon to="/" color="white">
         <v-icon>home</v-icon>
       </v-btn>
-      <v-toolbar-title>TREVI</v-toolbar-title>
+      <v-toolbar-title class="white--text">TRAVI</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-if="isLogin" @click.stop="dialog = true">LOGOUT</v-btn>
-        <v-btn flat @click.stop="dialog = true">LOGIN</v-btn>
-        
-        <v-dialog v-model="dialog" max-width="290">
-          <v-card>
-            <v-card-title class="headline">Log in</v-card-title>
->>>>>>> d618d41405c4d458e19b4d92a5ec56e9f0db583b
-
+        <!-- 로그인 다이얼로그 활성화 -->
+        <v-btn flat color="white" @click.stop="login_btn = true">Login</v-btn>
+          <v-dialog v-model="login_btn" max-width="290">
+            <v-card>
+              <v-card-title class="headline">Log in</v-card-title>
               <v-card-text>
                 <SignIn></SignIn>
-                <!-- SignIn template -->
+                SignIn template
               </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Disagree</v-btn>
+                <v-btn color="primary darken-1" flat="flat" @click="login_btn = false">Agree</v-btn>
+              </v-card-actions>
             </v-card>
           </v-dialog>
-          <v-menu offset-y>
-            <template v-slot:activator="{ on }">
-              <v-btn v-on="on" flat>Writer</v-btn>
-            </template>
-            <v-list>
-              <v-list-tile v-for="(item, index) in items" :key="index" @click>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-menu>
-          <v-btn flat>Search</v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-    </div>
-  </nav>
+        <v-btn to="/post" class="white--text" flat>Post</v-btn>
+        <v-btn to="/portfolio" class="white--text" flat>Portfolio</v-btn>
+        <!-- sdfsdfsdfsdfsd-->
+<template>
+  <v-card color="grey lighten-4" flat height="200px">
+    <v-toolbar color="grey darken-1" dark>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-menu :nudge-width="100">
+        <template v-slot:activator="{ on }">
+          <v-toolbar-title v-on="on">
+            <span>All</span>
+            <v-icon dark>arrow_drop_down</v-icon>
+          </v-toolbar-title>
+        </template>
+
+        <v-list>
+          <v-list-tile
+            v-for="item in items"
+            :key="item"
+            @click=""
+          >
+            <v-list-tile-title v-text="item"></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+
+        <!-- <v-dialog v-model="dialog" width="500"> -->
+          <!-- <template v-slot:activator="{ on }">
+            <v-btn class="white--text" flat v-on="on">
+              <v-icon >star</v-icon>
+            </v-btn>
+          </template> -->
+          <!-- <v-card>
+            <v-card-title class="headline primary lighten-3 white--text">
+              안내
+            </v-card-title>
+            <v-card-text>
+              즐겨찾기에 추가하려면 Ctrl+D를 누르세요
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary lighten-3" class="white--text" @click="dialog = false">확인</v-btn>
+            </v-card-actions>
+          </v-card> -->
+        <!-- </v-dialog> -->
+      </v-toolbar-items>
+      <v-toolbar-side-icon class="hidden-sm-and-up white--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+
+    <!-- <v-navigation-drawer app right disable-resize-watcher v-model="drawer" class="primary lighten-3">
+      <v-list>
+        <v-list-tile @click="$router.push('/post')">
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text title">Post</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$router.push('/portfolio')">
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text title">Portfolio</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="$router.push('/login')">
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text title">Login</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer> -->
+</div>
 </template>
 
 <script>
@@ -131,8 +111,14 @@ export default {
       { title: "mansub" },
       { title: "rain" }
     ],
-    dialog: false
+    dialog: false,
+    login_btn: false
   }),
+  methods:{
+    showLoginDialog:function(){
+
+    }
+  },
   mounted: function(){
     console.log(sessionStorage['length'])
     let isLogin = sessionStorage['length']
