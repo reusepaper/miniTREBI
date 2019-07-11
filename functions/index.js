17f28f6dd83682f8a420a48714fd3c7988966b0d
leeiopd <leeiopd@hanmail.net>
2019-07-11 15:35:39 +0900

    ho|page log cors error

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,42 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 exports.home = functions.https.onRequest((req, res) => {
-  res.status(200).send("home");
+  cors(request, response, () => {
+    res.status(200).send("home");
+  });
 });
 
 exports.post = functions.https.onRequest((req, res) => {
-  res.status(200).send("post");
+  cors(request, response, () => {
+    res.status(200).send("post");
+  });
 });
 
 exports.portfolio = functions.https.onRequest((req, res) => {
-  res.status(200).send("portfolio");
+  cors(request, response, () => {
+    res.status(200).send("portfolio");
+  });
 });
 
-exports.creatPost = functions.https.onRequest((req, res) => {
-  res.status(200).send("creatpost");
+exports.creatpost = functions.https.onRequest((req, res) => {
+  cors(request, response, () => {
+    res.status(200).send("creatpost");
+  });
 });
 
 exports.login = functions.https.onRequest((req, res) => {
-  res.status(200).send("login");
+  cors(request, response, () => {
+    res.status(200).send("login");
+  });
 });
-
 exports.logout = functions.https.onRequest((req, res) => {
-  res.status(200).send("logout");
+  cors(request, response, () => {
+    res.status(200).send("logout");
+  });
 });
