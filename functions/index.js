2e2334e8eb007a392e6524630459d5f960bf7968
leeiopd <leeiopd@hanmail.net>
2019-07-11 16:57:00 +0900

    ho|merge

--- functions/index.js
+++ functions/index.js
@@ -1,60 +1,44 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
-const cors = require("cors")({ origin: true });
+const cors = require("cors")({
+  origin: true
+});
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.home = functions.https.onRequest((req, res) => {
+exports.home = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("home");
+    response.status(200).send("home");
   });
 });
 
-exports.post = functions.https.onRequest((req, res) => {
+exports.post = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("post");
+    response.status(200).send("post");
   });
 });
 
-exports.portfolio = functions.https.onRequest((req, res) => {
+exports.portfolio = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("portfolio");
+    response.status(200).send("portfolio");
   });
 });
 
-exports.creatpost = functions.https.onRequest((req, res) => {
+exports.creatpost = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("creatpost");
+    response.status(200).send("creatpost");
   });
 });
 
-exports.login = functions.https.onRequest((req, res) => {
+exports.login = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("login");
+    response.status(200).send("login");
   });
 });
-exports.logout = functions.https.onRequest((req, res) => {
+exports.logout = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    res
-      .set({ "Access-Control-Allow-Origin": "*" })
-      .status(200)
-      .send("logout");
+    response.status(200).send("logout");
   });
 });
