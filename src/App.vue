ab02120b94dcd8ef532de0c575041fef96f65992
youdongkwan <dkyou7@naver.com>
2019-07-11 09:25:29 +0900

    YDK | floatin icon position 120px up

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <WMHeader></WMHeader>
       <router-view />
       <back-to-top text="Back to top" visibleoffset="100">
-        <button type='button' class='v-btn v-btn--bottom v-btn--floating v-btn--fixed v-btn--right theme--dark light-blue'>
+        <button type='button' style='bottom:120px;' class='ydk v-btn v-btn--bottom v-btn--floating v-btn--fixed v-btn--right theme--dark light-blue'>
           <i aria-hidden='true' class='v-icon material-icons theme--dark'>keyboard_arrow_up</i>
         </button>
       </back-to-top>
@@ -37,3 +37,6 @@ export default {
   }
 };
 </script>
+<style>
+
+</style>
