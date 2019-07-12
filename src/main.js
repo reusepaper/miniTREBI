e968a1ade0f6a44f023a734590be8e631a82af91
leeiopd <leeiopd@hanmail.net>
2019-07-12 15:01:46 +0900

    ho|faivcon 수정

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import Vuetify from "vuetify";
 import VueSimplemde from "vue-simplemde";
 import "simplemde/dist/simplemde.min.css";
 
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/firebase-functions";
+import "firebase/storage";
+
 import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
@@ -15,6 +22,7 @@ import VueAxios from "vue-axios";
 Vue.use(VueAxios, axios);
 Vue.use(BackToTop);
 Vue.use(browserDetect);
+Vue.use(firebase);
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
