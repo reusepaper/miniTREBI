fbc79bc90940eccf420c8c89869d397cc61f77cc
한만섭 <13akstjq@naver.com>
2019-07-15 14:10:40 +0900

    add default profile

--- src/components/EditProfileModal.vue
+++ src/components/EditProfileModal.vue
@@ -37,8 +37,7 @@ export default {
   data() {
     return {
       writer: "",
-      image:
-        "https://scontent-nrt1-1.cdninstagram.com/vp/14e487ffcb73b4d07dd6cf3dd7688afb/5DA39AF1/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com"
+      image: this.$store.state.profileImage
     };
   },
   mounted() {
@@ -51,6 +50,7 @@ export default {
   methods: {
     upLoadImg(image) {
       this.image = image;
+      this.$store.state.profileImage = this.image;
     }
   }
 };
