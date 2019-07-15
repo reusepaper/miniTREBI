6c42671ccd2c9bbd0c411ea6f9ab546c30cb534b
leeiopd <leeiopd@hanmail.net>
2019-07-15 14:02:27 +0900

    ho|weather google geocoding 사용
    google geocoding api를 사용하여
    위도, 경도 정보로 현재 주소 위치를 출력

--- src/components/WMFooter.vue
+++ src/components/WMFooter.vue
@@ -79,7 +79,6 @@ export default {
       navigator.geolocation.getCurrentPosition(position => {
         const lat = position.coords.latitude;
         const log = position.coords.longitude;
-        console.log(lat, log);
         this.getWeather(lat, log);
         this.getLocation(lat, log);
       });
@@ -93,60 +92,20 @@ export default {
         .then(json => {
           this.weather = json.weather[0].main;
           this.temp = Math.floor(json.main.temp - 273.15);
-          if (json.name === "Taejŏn-gwangyŏksi") {
-            this.place = "대전광역시";
-          }
           /*eslint-disable*/
           console.log(this.weather);
           // console.log(this.temp, this.place, this.weather);
         });
     },
-    getLocastion: function(lat, log) {
-      var geocode =
-        "http://maps.googleapis.com/maps/api/geocode/json?latlng=" +
-        lat +
-        "," +
-        log +
-        "&sensor=false";
-      jQuery.ajax({
-        url: geocode,
-        type: "POST",
-        success: function(myJSONResult) {
-          if (myJSONResult.status == "OK") {
-            var tag = "";
-            var i;
-            for (i = 0; i < myJSONResult.results.length; i++) {
-              tag +=
-                "주소 : " +
-                myJSONResult.results[i].formatted_address +
-                " <br />";
-              tag +=
-                "위도 : " +
-                myJSONResult.results[i].geometry.location.lat +
-                " <br />";
-              tag +=
-                "경도 : " +
-                myJSONResult.results[i].geometry.location.lng +
-                " <br />";
-            }
-            document.getElementById("message").innerHTML = tag;
-          } else if (myJSONResult.status == "ZERO_RESULTS") {
-            alert(
-              "지오코딩이 성공했지만 반환된 결과가 없음을 나타냅니다.\n\n이는 지오코딩이 존재하지 않는 address 또는 원격 지역의 latlng을 전달받는 경우 발생할 수 있습니다."
-            );
-          } else if (myJSONResult.status == "OVER_QUERY_LIMIT") {
-            alert("할당량이 초과되었습니다.");
-          } else if (myJSONResult.status == "REQUEST_DENIED") {
-            alert(
-              "요청이 거부되었습니다.\n\n대부분의 경우 sensor 매개변수가 없기 때문입니다."
-            );
-          } else if (myJSONResult.status == "INVALID_REQUEST") {
-            alert(
-              "일반적으로 쿼리(address 또는 latlng)가 누락되었음을 나타냅니다."
-            );
-          }
-        }
-      });
+    getLocation: function(lat, log) {
+      fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${log}&key=AIzaSyC1x8tesUWMdo3VoAH3zpj56H2qw47PF4k`
+      )
+        .then(response => response.json())
+        .then(
+          json =>
+            (this.place = json.results[0].address_components[3].short_name)
+        );
     }
   }
 };
