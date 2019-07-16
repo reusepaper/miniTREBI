c2463a7430f7da8ab329f850b1c62fcb0d9e2526
한만섭 <13akstjq@naver.com>
2019-07-16 14:41:48 +0900

    mansub | modify sidebar profile

--- src/store.js
+++ src/store.js
@@ -9,10 +9,11 @@ export default new Vuex.Store({
     postWriter: "",
     content: "",
     image: "",
-    profileImage:
+    defaultImage:
       "https://scontent-nrt1-1.cdninstagram.com/vp/14e487ffcb73b4d07dd6cf3dd7688afb/5DA39AF1/t51.2885-19/44884218_345707102882519_2446069589734326272_n.jpg?_nc_ht=scontent-nrt1-1.cdninstagram.com",
     seePostId: "",
-    writer: ""
+    writer: "",
+    user: null
   },
 
   mutations: {
@@ -24,6 +25,12 @@ export default new Vuex.Store({
     },
     setUser(state, currentUser) {
       state.user = currentUser;
+    },
+    setUserImage(state, newImage) {
+      state.user.photoURL = newImage;
+    },
+    setUserDisplayName(state, newName) {
+      state.user.displayName = newName;
     }
   },
   actions: {},
